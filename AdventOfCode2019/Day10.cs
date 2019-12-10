7f74895a675478db991bab71397cf499b565923d
Vern Fridell <vfridell@bellarmine.edu>
2019-12-10 18:44:33 -0500

    ugh part 2

--- AdventOfCode2019/Day10.cs
+++ AdventOfCode2019/Day10.cs
@@ -10,15 +10,20 @@ namespace AdventOfCode2019
     {
         public static List<string> Input = new List<string>()
         {
-            "......#.#.","#..#.#....","..#######.",".#.#.###..",".#..#.....","..#....#.#","#..#....#.",".##.#..###","##...#..#.",".#....####"
+           // "#..#.#.#.######..#.#...##","##.#..#.#..##.#..######.#",".#.##.#..##..#.#.####.#..",".#..##.#.#..#.#...#...#.#","#...###.##.##..##...#..#.","##..#.#.#.###...#.##..#.#","###.###.#.##.##....#####.",".#####.#.#...#..#####..#.",".#.##...#.#...#####.##...","######.#..##.#..#.#.#....","###.##.#######....##.#..#",".####.##..#.##.#.#.##...#","##...##.######..##..#.###","...###...#..#...#.###..#.",".#####...##..#..#####.###",".#####..#.#######.###.##.","#...###.####.##.##.#.##.#",".#.#.#.#.#.##.#..#.#..###","##.#.####.###....###..##.","#..##.#....#..#..#.#..#.#","##..#..#...#..##..####..#","....#.....##..#.##.#...##",".##..#.#..##..##.#..##..#",".##..#####....#####.#.#.#","#..#..#..##...#..#.#.#.##"
+           
+            // ".#....#####...#..","##...##.#####..##","##...#...#.#####.","..#.....X...###..","..#.#.....#....##"
+".#..##.###...#######","##.############..##.",".#.######.########.#",".###.#######.####.#.","#####.##.#.##.###.##","..#####..#.#########","####################","#.####....###.#.#.##","##.#################","#####.##.###..####..","..######..##.#######","####.##.####...##..#",".#####..#.######.###","##...#.##########...","#.##########.#######",".####.#.###.###.#.##","....##.##.###..#####",".#.#.###########.###","#.#.#.#####.####.###","###.##.####.##.#..##"
+
         };
+        public enum Position { Left, Right};
 
         public static void Part1()
         {
 
             Dictionary<Coordinate, char> map = new Dictionary<Coordinate, char>();
-            Coordinate coord = new Coordinate(0,0);
-            foreach(string s in Input)
+            Coordinate coord = new Coordinate(0, 0);
+            foreach (string s in Input)
             {
                 int i = 0;
                 foreach (char c in s)
@@ -30,37 +35,95 @@ namespace AdventOfCode2019
             }
 
             List<Coordinate> Asteroids = map.Where(kvp => kvp.Value != '.').Select(kvp => kvp.Key).ToList();
-            //Dictionary<Coordinate, List<Func<Coordinate, bool>>> rays = new Dictionary<Coordinate, List<Func<Coordinate, bool>>>();
-            Dictionary<Coordinate, Dictionary<Coordinate,double>> coordinateSlopes = new Dictionary<Coordinate, Dictionary<Coordinate, double>>();
+            Dictionary<Coordinate, Dictionary<Coordinate, (double, Position)>> coordinateSlopes = new Dictionary<Coordinate, Dictionary<Coordinate, (double, Position)>>();
             foreach (Coordinate a1 in Asteroids)
             {
-                coordinateSlopes.Add(a1, new Dictionary<Coordinate, double>());
-                //List<Func<Coordinate, bool>> myRays = new List<Func<Coordinate, bool>>();
-                //rays.Add(a1, myRays);
+                coordinateSlopes.Add(a1, new Dictionary<Coordinate, (double, Position)>());
                 foreach (Coordinate a2 in Asteroids)
                 {
                     if (a1 == a2) continue;
                     double m = (double)(a2.Row - a1.Row) / (double)(a2.Column - a1.Column);
-
-                    coordinateSlopes[a1].Add(a2, m);
-                    //myRays.Add((c) =>
-                    //{
-                    //    if (double.IsInfinity(m))
-                    //        return c.Column == a1.Column;
-                    //    else
-                    //        return c.Row - a1.Row == m * (c.Column = a1.Column);
-                    //});
+                    Position pos = (a2.Column < a1.Column) ? Position.Left : Position.Right;
+                    coordinateSlopes[a1].Add(a2, (m, pos));
                 }
             }
 
-            int maxVisibleAsteroids = coordinateSlopes.Max(kvp => kvp.Value.Count);
-            int count = coordinateSlopes[new Coordinate(8, 5)].Select(kvp => kvp.Value).Distinct().Count();
+            IEnumerable<(Coordinate, int)> visibleAsteroids = coordinateSlopes.Select(kvp => (kvp.Key, kvp.Value.GroupBy(kvp2 => kvp2.Value).Count()));
 
-            Coordinate answer = new Coordinate(8, 5);
-            List<Coordinate> visibleFromAnswer = new List<Coordinate>();
-            foreach (var group in coordinateSlopes[answer].GroupBy(kvp => kvp.Value))
+            (Coordinate answer, int maxVisibleAsteroids) = visibleAsteroids.OrderByDescending(t => t.Item2).First();
+            Console.WriteLine($"Best Coordinate {answer} has {maxVisibleAsteroids} asteroids visible");
+        }
+
+        public static void Part2()
+        {
+
+            Dictionary<Coordinate, char> map = new Dictionary<Coordinate, char>();
+            Coordinate coord = new Coordinate(0, 0);
+            foreach (string s in Input)
             {
-                visibleFromAnswer.Add(group.OrderBy(kvp => Coordinate.Distance(answer, kvp.Key)).First().Key);
+                int i = 0;
+                foreach (char c in s)
+                {
+                    map.Add(coord, s[i++]);
+                    coord = coord + new Coordinate(0, 1);
+                }
+                coord = new Coordinate(coord.Row + 1, 0);
+            }
+
+            List<Coordinate> Asteroids = map.Where(kvp => kvp.Value != '.').Select(kvp => kvp.Key).ToList();
+            Dictionary<Coordinate, Dictionary<Coordinate, (double, Position)>> coordinateSlopes = new Dictionary<Coordinate, Dictionary<Coordinate, (double, Position)>>();
+            foreach (Coordinate a1 in Asteroids)
+            {
+                coordinateSlopes.Add(a1, new Dictionary<Coordinate, (double, Position)>());
+                foreach (Coordinate a2 in Asteroids)
+                {
+                    if (a1 == a2) continue;
+                    double m = (double)(a2.Row - a1.Row) / (double)(a2.Column - a1.Column);
+                    Position pos = (a2.Column < a1.Column) ? Position.Left : Position.Right;
+                    coordinateSlopes[a1].Add(a2, (m, pos));
+                }
+            }
+
+            IEnumerable<(Coordinate, int)> visibleAsteroids = coordinateSlopes.Select(kvp => (kvp.Key, kvp.Value.GroupBy(kvp2 => kvp2.Value).Count()));
+            (Coordinate answer, int maxVisibleAsteroids) = visibleAsteroids.OrderByDescending(t => t.Item2).First();
+            Console.WriteLine($"Best Coordinate {answer} has {maxVisibleAsteroids} asteroids visible");
+
+            var righties = coordinateSlopes[answer]
+                            .Where(kvp => kvp.Value.Item2 == Position.Right)
+                            .OrderBy(kvp => kvp.Value.Item1)
+                            .ThenBy(kvp => Coordinate.Distance(answer, kvp.Key));
+            var lefties = coordinateSlopes[answer]
+                            .Where(kvp => kvp.Value.Item2 == Position.Left)
+                            .OrderByDescending(kvp => kvp.Value.Item1)
+                            .ThenBy(kvp => Coordinate.Distance(answer, kvp.Key));
+
+            var all = righties.Concat(lefties).GroupBy(kvp => kvp.Value);
+            List<Coordinate> blowedUp = new List<Coordinate>();
+            while (true)
+            {
+                foreach (var group in all)
+                {
+                    if (group.Select(kvp => kvp.Key).Any(c => !blowedUp.Contains(c)))
+                    {
+                        var asteeroidToBlowUp = group.Select(kvp => kvp.Key).FirstOrDefault(c => !blowedUp.Contains(c));
+                        blowedUp.Add(asteeroidToBlowUp);
+                        Console.WriteLine($"blowed up {asteeroidToBlowUp}");
+                        if(blowedUp.Count == 200)
+                        {
+                            Console.WriteLine($"200th blowed up is {asteeroidToBlowUp}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void DisplayVisibleFromCoordinate(Dictionary<Coordinate, char> map, Dictionary<Coordinate, Dictionary<Coordinate, (double, Position)>> coordinateSlopes, Coordinate inputCoordinate)
+        {
+            Coordinate coord;
+            List<Coordinate> visibleFromAnswer = new List<Coordinate>();
+            foreach (var group in coordinateSlopes[inputCoordinate].GroupBy(kvp => kvp.Value))
+            {
+                visibleFromAnswer.Add(group.OrderBy(kvp => Coordinate.Distance(inputCoordinate, kvp.Key)).First().Key);
             }
 
             coord = new Coordinate(0, 0);
@@ -70,7 +133,7 @@ namespace AdventOfCode2019
                 foreach (char c in s)
                 {
                     if (visibleFromAnswer.Contains(coord)) Console.Write("%");
-                    else if (answer == coord) Console.Write("!");
+                    else if (inputCoordinate == coord) Console.Write("!");
                     else Console.Write(map[coord]);
                     coord = coord + new Coordinate(0, 1);
                 }
@@ -78,12 +141,7 @@ namespace AdventOfCode2019
                 coord = new Coordinate(coord.Row + 1, 0);
             }
 
-            Coordinate bestCoord = coordinateSlopes.Where(kvp => kvp.Value.Count == maxVisibleAsteroids).Single().Key;
-            Console.WriteLine($"Best Coordinate {bestCoord} has {maxVisibleAsteroids} asteroids visible");
-
+            return;
         }
-
-
-
     }
 }
--- AdventOfCode2019/Program.cs
+++ AdventOfCode2019/Program.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode2019
     {
         static void Main(string[] args)
         {
-            Day10.Part1();
+            Day10.Part2();
         }
     }
 }
